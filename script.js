8c36133cb9c75854aee3ff5d6c7a6edf98535eac
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-10-27 20:13:18 +0600

    fetch api

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ counter = counter + 1;
 document.getElementById('score').innerText = counter;
 };
 
+
+fetch('https://reqres.in/api/users',{
+    method : 'POST',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+})
+.then(res => {
+    // if (res.ok){
+    //     console.log('SUCCESS')
+    // }else{
+    //     console.log("Not successful")
+    // }
+    return res.json( )
+})
+
+.then(data => console.log(data))
+.catch(error => console.log('ERROR'))
