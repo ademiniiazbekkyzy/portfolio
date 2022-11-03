0f3a19a053620d7389cf006562cb3741a1e515c3
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-11-03 09:52:21 +0600

    updated to meet the requirements

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-let counter = 0;
-document.getElementById('button').onclick = () => {
-counter = counter + 1;
-document.getElementById('score').innerText = counter;
-};
+const header = document.querySelector("header");
 
-
-fetch('https://reqres.in/api/users',{
-    method : 'POST',
-    headers: {
-        'Content-Type':'application/json'
-    },
-    body: JSON.stringify({
-        name: 'User 1'
-    })
-})
-.then(res => {
-    // if (res.ok){
-    //     console.log('SUCCESS')
-    // }else{
-    //     console.log("Not successful")
-    // }
-    return res.json( )
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky", window.scrollY>0);
 })
 
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
+var oddtrivia = []
+oddtrivia[0] = "There are around 700 separate programming languages";
+oddtrivia[1] = "The world’s first computer programmer was a renowned female mathematician";
+oddtrivia[2] = "Computer Programming was instrumental in helping end World War II";
+oddtrivia[3] = "Many owners of large tech companies loved video games as kids";
+oddtrivia[4] = "The first-ever computer game made zero profit for its team of creators";
+oddtrivia[5] = "NASA still uses programs from the 70s in their spacecraft";
+
+function oddTrivia(){
+    var randomTrivia = Math.floor(Math.random()*(oddtrivia.length));
+    document.getElementById('ptrivia').innerHTML = oddtrivia[randomTrivia];
+}
+
+
