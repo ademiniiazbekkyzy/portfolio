0294cf9be6ebe9cfb58163b7956e597bdc22e661
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-11-03 22:35:59 +0600

    updated to meet the requirements2

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ oddtrivia[2] = "Computer Programming was instrumental in helping end World War I
 oddtrivia[3] = "Many owners of large tech companies loved video games as kids";
 oddtrivia[4] = "The first-ever computer game made zero profit for its team of creators";
 oddtrivia[5] = "NASA still uses programs from the 70s in their spacecraft";
-
+oddtrivia[6] = "The first programming language in the world was called FORTRAN ";
+oddtrivia[7] = "HP, Microsoft, and Apple all began in a garage.";
+oddtrivia[8] = "Hackers Write About 6,000 New Viruses Each Month";
+oddtrivia[9] = " MIT Has Computers That can Detect Fake Smiles";
 function oddTrivia(){
     var randomTrivia = Math.floor(Math.random()*(oddtrivia.length));
     document.getElementById('ptrivia').innerHTML = oddtrivia[randomTrivia];
